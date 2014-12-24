dc4035b42728c362f4da3f385e0cfe359c2d9c4f
Dan Sweeney <sweeneyd@vt.edu>
2014-12-24 12:51:00 -0500

    Ellipse fitting update
    Error with errant fitting—need to fix in particle tracking

--- video.py
+++ video.py
@@ -37,23 +37,50 @@ def trimEdges(raw, background):
     b[indices] = 0
     return b
 
-def getContours(img):
+def getContours(img, fit_type = 'ellipse'):
+    CELL_RADIUS_THRESHOLD = 4
     contours, hierarchy = cv2.findContours(img,
                                            cv2.RETR_TREE,
                                            cv2.CHAIN_APPROX_SIMPLE)
     img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-    radius_list = []
-    x_list = []
-    y_list = []
+    coord_list = []
     for i in range(len(contours)):
-        (x,y),radius = cv2.minEnclosingCircle(contours[i])
-        if radius > 5:
-            center = (int(x),int(y))
-            radius_list.append(radius)
-            x_list.append(x)
-            y_list.append(y)
-            cv2.circle(img,center,int(radius),(0,255,0),2)
-    return img, contours, radius, (x, y)
+        if fit_type == 'circle':
+            radius_list = []
+            center_list = []
+            (x,y),radius = cv2.minEnclosingCircle(contours[i])
+            if radius > CELL_RADIUS_THRESHOLD:
+                center = (int(x), int(y))
+                center_list.append(center)
+                radius_list.append(radius)
+                cv2.circle(img,center,int(radius),(0,255,0),-11)
+            coord_list.append([center_list, radius_list])
+            
+        elif fit_type == 'ellipse':
+            if len(contours[i]) >= 5:
+                ellipse = cv2.fitEllipse(contours[i])
+                area = np.pi*np.product(ellipse[1])
+                if area >= 200 and area < 2000:
+                    cv2.ellipse(img,ellipse,(0,255,0),-1)
+    return img, contours, coord_list
+    
+def getFiltered(img, CELL_BINARY_THRESHOLD = 127):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    subBG = trimEdges(gray, meds)
+    thresh = cv2.erode(subBG, None, 10)
+    thresh = cv2.dilate(thresh, None, 10)
+    ret,thresh = cv2.threshold(subBG,CELL_BINARY_THRESHOLD,255,0)
+    thresh, contours, coord_list = getContours(thresh)
+    return thresh, contours, coord_list
+    
+def getCanny(img, CELL_BINARY_THRESHOLD = 127):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    subBG = trimEdges(gray, meds)
+    thresh = cv2.erode(subBG, None, 10)
+    thresh = cv2.dilate(thresh, None, 10)
+    ret,thresh = cv2.threshold(subBG,CELL_BINARY_THRESHOLD,255,0)
+    thresh = cv2.Canny(thresh, 5, 10)
+    return thresh
     
 # ============================================================================
 #                           Video Analysis
@@ -61,25 +88,23 @@ def getContours(img):
 
 #cap = cv2.VideoCapture('/Users/nicobaudoin/Desktop/DavalosLab/testvid.avi')
 #cap = cv2.VideoCapture('/Users/Dan/Movies/Mitosis in 2D - HeLa cell undergoes mitosis-HD.mp4/')
-cap = cv2.VideoCapture('/Users/Dan/Movies//nprot.mp4')
+cap = cv2.VideoCapture('/Users/Dan/Movies/nprot.mp4')
 meds = getMedian(cap)
 
 while cap.isOpened():
-    
     try:
         ret, frame = cap.read()
-        if frame == None:
+        if frame != None:
+            thresh, contours, coord_list = getFiltered(frame)
+            #thresh = getCanny(frame) 
+        else:
             break
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        subBG = trimEdges(gray, meds)
-        ret,thresh = cv2.threshold(subBG,127,255,0)
-        thresh, contours, radius, (x, y) = getContours(thresh)
         cv2.imshow('thresh', thresh)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             cap.release()
             cv2.destroyAllWindows()
             break
-            
+        
     except KeyboardInterrupt:
         cv2.destroyAllWindows()
         break
